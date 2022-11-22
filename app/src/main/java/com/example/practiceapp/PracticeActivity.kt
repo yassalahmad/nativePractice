2410e0532d59b0dee367ef31db12d4b459432e6b
yassalahmad <yassal.qureshi@gmail.com>
2022-11-22 15:26:23 +0500

    November 22 2022

--- app/src/main/java/com/example/practiceapp/PracticeActivity.kt
+++ app/src/main/java/com/example/practiceapp/PracticeActivity.kt
@@ -1,11 +1,21 @@
 package com.example.practiceapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.practiceapp.databinding.ActivityPracticeBinding
 
 class PracticeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityPracticeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_practice)
+        binding= ActivityPracticeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener { startActivity(Intent(this,MainActivity::class.java)) }
+
+//        var back = findViewById<Button>(R.id.button)
+//        back.setOnClickListener { startActivity(Intent(this,MainActivity::class.java)) }
     }
 }
